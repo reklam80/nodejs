e4ffd24a3468da27e5b3ba489dbd571f168e51f8
Serdar Yakisan <reklam80@gmail.com>
2018-12-03 19:18:16 +0300

    port değişikliği

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var middleware = require("./middleware");
-var port = process.env.port || 80;
+var PORT = process.env.port || 3000;
 
 app.use(middleware.logger);
 
@@ -11,6 +11,7 @@ app.get("/hakkimda", middleware.requireAutentication, function(req,res){
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-    console.log("Express server " + port + " nolu portta çalışıyor..");
+app.listen(PORT, function(){
+    //var port = server.address().port;
+    console.log("Express server " + PORT +  " nolu portta çalışıyor..",this.address().PORT, app.settings.env);
 });
